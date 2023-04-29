05b8a334e42526f7e828f9d7b2dc329089ed7789
Joszko <jakub.rezler96@gmail.com>
2023-04-29 02:08:15 +0200

    create setOverPrintLocation object

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ const inputs = document.querySelectorAll(".checkboxes-list__checkbox");
 const imgPreview = document.querySelector(
 	".checkboxes-and-preview__overprint-preview"
 );
-
-inputs.forEach((e) => {
-	e.addEventListener("change", (el) => {
-		const overPrintLocation = el.target.getAttribute("data-location");
+const setOverPrintLocation = {
+	location: "front",
+	setImage: (item) => {
+		const overPrintLocation = item.target.getAttribute("data-location");
 		if (overPrintLocation === "front") {
 			imgPreview.src = "https://picsum.photos/id/120/200/200";
 		} else if (overPrintLocation === "back") {
@@ -13,5 +13,9 @@ inputs.forEach((e) => {
 		} else if (overPrintLocation === "front&back") {
 			imgPreview.src = "https://picsum.photos/id/122/200/200";
 		}
-	});
+	},
+};
+
+inputs.forEach((e) => {
+	e.addEventListener("change", (item) => setOverPrintLocation.setImage(item));
 });
