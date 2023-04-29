f06e82f8ab28fd8b1c8639e6915c37fb5cf8fd27
Joszko <jakub.rezler96@gmail.com>
2023-04-30 01:45:58 +0200

    create effect choice mechanics

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-const inputs = document.querySelectorAll(".checkboxes-list__checkbox");
+const inputsChooseLocation = document.querySelectorAll(
+	".choose-print-location .checkboxes-list__checkbox"
+);
+const inputsChooseEffect = document.querySelectorAll(
+	".choose-effect .checkboxes-list__checkbox"
+);
 const imgPreview = document.querySelector(".print-preview__print-img");
 const frontShirt = document.querySelector(".print-preview__shirt-front");
 const backShirt = document.querySelector(".print-preview__shirt-back");
@@ -15,17 +20,17 @@ class Page {
 		if (btn.target.name === "next") {
 			pageButtonBack.classList.remove("navigate-buttons__btn--disabled");
 			pages.activePage += 1;
-			console.log(pages);
 		} else if (btn.target.name === "back" && pages.activePage > 2) {
 			pages.activePage -= 1;
-			console.log(pages);
 		} else {
 			pages.activePage -= 1;
 			pageButtonBack.classList.add("navigate-buttons__btn--disabled");
-			console.log(pages);
 		}
 		changePageDisplay(pages.activePage - 1);
 		updateSummaryDetails();
+		if (pages.activePage >= 3) {
+			product1.setEffectPreviewImg();
+		}
 	}
 }
 
@@ -55,6 +60,7 @@ class Product {
 	setPrintLocation(location) {
 		this.printLocation = location;
 		this.setPrice();
+		updateSummaryDetails();
 	}
 
 	setLocationPreviewImg() {
@@ -73,15 +79,36 @@ class Product {
 		}
 	}
 
+	setEffectPreviewImg() {
+		const effectPreviewImg = document.querySelector("#effect-preview");
+		let effect;
+		if (product1.effect === "Czarno-białe") {
+			effect = "?grayscale";
+		} else if (product1.effect === "Rozmycie") {
+			effect = "?blur";
+		} else {
+			effect = "";
+		}
+		effectPreviewImg.src = `https://picsum.photos/id/${product1.graphics}/170/170${effect}`;
+		updateSummaryDetails(effect);
+	}
+
 	setGraphics(e) {
 		product1.graphics += e.target.name === "right" ? 1 : -1;
 		const graphicsImg = document.querySelector(".choose-graphics-img");
 		graphicsImg.src = `https://picsum.photos/id/${product1.graphics}/170/170`;
+		updateSummaryDetails();
+	}
+
+	setEffect(effect) {
+		product1.effect = effect;
+		this.setPrice();
+		updateSummaryDetails();
 	}
 
 	setPrice() {
 		this.price = this.printLocation === "Przód i tył" ? 20 : 10;
-		if (this.effect === "Czarno-biały") {
+		if (this.effect === "Czarno-białe") {
 			this.price += 2;
 		} else if (this.effect === "Rozmycie") {
 			this.price += 3;
@@ -96,7 +123,7 @@ class SummaryDetails {
 		this.productDetails = product;
 	}
 
-	updateDetails() {
+	updateDetails(effectURLcode) {
 		const { printLocation, effect, price } = this.productDetails;
 		const summaryLocationSpan = document.querySelector(
 			".summary-preview__location"
@@ -105,7 +132,6 @@ class SummaryDetails {
 			".summary-preview__effect"
 		);
 		const summaryCostSpan = document.querySelector(".summary-preview__cost");
-
 		const printPreview = document.querySelector(".print-preview");
 		const copyOfPrintPreview = printPreview.cloneNode(true);
 
@@ -113,13 +139,23 @@ class SummaryDetails {
 		summaryEffectSpan.innerHTML = effect;
 		summaryCostSpan.innerHTML = `${price} PLN`;
 
-		const summaryPreview = document.querySelector(".print-summary-preview");
-		const summaryToRemove = summaryPreview.querySelector(".print-preview");
-		if (pages.activePage >= 2) {
-			summaryPreview.appendChild(copyOfPrintPreview);
+		// set print preview in summary section
+		const summaryPrintPreview = document.querySelector(
+			".print-summary-preview"
+		);
+		const summaryToRemove = summaryPrintPreview.querySelector(".print-preview");
+		if (pages.activePage >= 2 && summaryToRemove === null) {
+			summaryPrintPreview.appendChild(copyOfPrintPreview);
 		} else if (pages.activePage === 1 && summaryToRemove !== null) {
-			summaryPreview.removeChild(summaryToRemove);
+			summaryPrintPreview.removeChild(summaryToRemove);
 		}
+		// set chosen graphics
+		const chosenGraphics = summaryPrintPreview.querySelectorAll("img");
+		const effectData = effectURLcode !== undefined ? effectURLcode : "";
+		chosenGraphics.forEach((e) => {
+			// console.log(effectURLcode);
+			e.src = `https://picsum.photos/id/${product1.graphics}/170/170${effectData}`;
+		});
 	}
 
 	updateCost(updateCost) {
@@ -127,19 +163,27 @@ class SummaryDetails {
 	}
 }
 const summary = new SummaryDetails(product1);
-summary.updateDetails();
+updateSummaryDetails();
 
-function updateSummaryDetails() {
-	summary.updateDetails();
+function updateSummaryDetails(effect) {
+	summary.updateDetails(effect);
 }
 
-inputs.forEach((e) => {
+// choose print location inputs on page 1
+inputsChooseLocation.forEach((e) => {
 	e.addEventListener("change", (item) => {
 		const location = item.target.getAttribute("data-location");
 		product1.setPrintLocation(location);
 		product1.setLocationPreviewImg();
-		summary.updateDetails();
-		console.log(product1);
+	});
+});
+
+// choose effect inputs on page 3
+inputsChooseEffect.forEach((e) => {
+	e.addEventListener("change", (item) => {
+		const effect = item.target.getAttribute("data-effect");
+		product1.setEffect(effect);
+		product1.setEffectPreviewImg();
 	});
 });
 
